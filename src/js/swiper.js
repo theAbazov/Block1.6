1391e44ad55efc5c56f81def60fbc41d8714604e
Isa <abazovkjks@gmail.com>
2022-09-06 15:47:43 +0600

    gh-pages

--- src/js/swiper.js
+++
@@ -1,9 +0,0 @@
-let swiper = new Swiper(window.innerWidth < 768 ? ".mySwiper" : "", {
-    slidesPerView: "auto",
-    spaceBetween: 16,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
-  
